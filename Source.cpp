17567beb2bc821e32149dd06583c3c9ccab2112e
Eugene <efoo10@gmail.com>
2018-10-16 16:40:21 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <stdio.h>
 
-#include <librealsense2\rs.hpp>
-#include "imgui\imgui.h"
-#include "imgui\imgui_impl_glfw.h"
-
 #include "displayfunctions.hpp"
+#include "imguifunctions.hpp"
+#include "rsgui.hpp"
 
 int main(int argc, char* argv[]) {
 	window_rs window(1280, 720, "IntelRealsenseXOneS");
@@ -15,14 +13,19 @@ int main(int argc, char* argv[]) {
 	rs2::pipeline pipe;
 	pipe.start();
 
+	static AppLog log;
+	bool show_color_camera = false;
+	bool show_depth_camera = false;
+	bool show_app_log = false;
+
+	bool button_pressed = false;
+
 	while (window) {
 		rs2::frameset data = pipe.wait_for_frames();
 		rs2::frame color = data.get_color_frame();
 		rgbPreview.render(color, { 0, 0, window.width()/2, window.height()/2 });
 
 		ImGui_ImplGlfw_NewFrame(1);
-		ImGui::Begin("HEllo");
-		ImGui::End();
-		ImGui::Render();
+		menuGUI(show_color_camera, show_depth_camera, show_app_log, button_pressed);
 	}
 }

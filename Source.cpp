4015f6e3008715a477fd0ff7451b2343bfcde166
Eugene <efoo10@gmail.com>
2018-10-16 14:14:41 +0800

    Fixed bugs

--- Source.cpp
+++ Source.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
-#include <thread>
 #include <stdio.h>
 
-#include "imgui_x.hpp"
-#include "realsense_gui_x.hpp"
-#include "displayhelper.hpp"
+#include <librealsense2\rs.hpp> // IMPORTANT I THINK
+
+#include "imgui\imgui.h" // ALSO IMPORTANT
+#include "imgui\imgui_impl_glfw.h"
+#include "realsense\example.hpp"
 
 int main(int argc, char* argv[]) {
-	//initGlfwRS();
-	window_rs window(1280, 720, "Realsense");
-	texture_rs color_image;
+	window window(1280, 720, "Realsense");
+	ImGui_ImplGlfw_Init(window, false);
 
-	//std::thread t1(&window_rs::showSimpleMenu, window);
-	//t1.join();
-	//window.showSimpleMenu();
-	
+	texture color_image;
 	rs2::pipeline pipe;
 	pipe.start();
 
 	while (window) {
 		rs2::frameset data = pipe.wait_for_frames();
 		rs2::frame color = data.get_color_frame();
-		color_image.render(color, { window.width() / 2, 0, window.width(), window.height() });
+		color_image.render(color, { 0, 0, window.width()/2, window.height()/2 });
 
 		ImGui_ImplGlfw_NewFrame(1);
-		ImGui::Begin("slider");
-
+		ImGui::Begin("HEllo");
 		ImGui::End();
 		ImGui::Render();
 	}

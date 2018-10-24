6b4599c8941821bb427ae73147c4ca2e1e341644
Eugene <efoo10@gmail.com>
2018-10-24 16:34:59 +0800

    Simplified Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,31 +1,14 @@
 #include <iostream>
 #include <stdio.h>
+#include <thread>
 
 #include "displayfunctions.hpp"
 #include "imguifunctions.hpp"
-
-class testx {
-public:
-	window_rs window;
-
-	void create() {
-		window.onCreate(1280, 720, "IntelRealsenseXOneS");
-		//window_rs window(1280, 720, "esgegx"); // create window
-		ImGui_ImplGlfw_Init(window, false);
-
-		texture_rs rgbPreview;
-		rs2::pipeline pipe;
-		pipe.start();
-
-		while (window) {
-			rs2::frameset data = pipe.wait_for_frames();
-			rs2::frame color = data.get_color_frame();
-			rgbPreview.render(color, { 0, 0, window.width() / 2, window.height() / 2 });
-		}
-	}
-};
+#include "rsgui.hpp"
 
 int main(int argc, char* argv[]) {
-	testx test;
-	test.create();
+	rsgui rsgui;
+
+	std::thread t1(&rsgui::init, rsgui);
+	t1.join();
 }

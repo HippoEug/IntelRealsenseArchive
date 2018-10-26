3bdb89ac6f30b13b1b8d7d1f0d944ea15dbbebc1
Eugene <efoo10@gmail.com>
2018-10-26 15:04:45 +0800

    Added a few comments

---
+++ V0.2.6/rsgui.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "displayfunctions.hpp" // Classes to create GLFW window
+
+class rsgui {
+public:
+	window_rs window; // Create object from Class window_rs
+
+	void init(); // Function init() prototype
+};

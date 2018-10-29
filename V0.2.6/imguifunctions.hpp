27569a758ce94a8426551a210dbfbecfb1265ad3
Eugene <efoo10@gmail.com>
2018-10-29 10:02:56 +0800

    Removed struct AppLog {}

---
+++ V0.2.6/imguifunctions.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "imgui\imgui.h"
+
+void menuGUI(bool& show_color_camera, bool& show_depth_camera, bool& camera_button, bool& coordinates_button, bool& CSV_button);
+void rosbagGUI();
